d143f97c746b2961b976394bb608047e1223ac36
Yuchen Liu <85184435+ycliu23@users.noreply.github.com>
2024-01-29 14:10:19 +0000

    Update cal_ps.py

--- pipeline/cal_ps.py
+++ pipeline/cal_ps.py
@@ -30,6 +30,21 @@ os.system('mkdir -p {}'.format(outpath))
 
 FREQ_TAPER = 'nuttall'
 ERROR_P1D = 1
+PREFIX = 'gpr_res'
+MASK = True
+THRESHOLD = 9
+
+K_PERP = np.linspace(5e-2,5e-1,10)
+K_PAR = np.linspace(5e-2,5e-1,10)
+
+def ratio(array):
+    ratios = np.zeros(len(array)-1)
+    for i in range(len(ratios)):
+        ratios[i] = array[i] / array[i+1]
+    return ratios
+
+def error_prop(array):
+    return np.sqrt(np.sum(array**2))
 
 res_cubes = []
 for i in os.listdir(inpath):
@@ -46,61 +61,63 @@ def cal_ps(file):
     eor_bin_list = pspec.EorBinList(data_cube.freqs)
     eor_bin_list.add_freq(1,freq_lower,freq_upper)
     eor = eor_bin_list.get(1)
-    el = 2 * np.pi * (np.arange(data_cube.ru.min(), data_cube.ru.max(), 10))
+    el = psutil.k_to_l(K_PERP,eor.z)
     ps_conf = pspec.PowerSpectraConfig(el,FREQ_TAPER)
     ps_conf.umax = data_cube.ru.max()
     ps_conf.umin = data_cube.ru.min()
     pb = datacube.SkaLowPrimaryBeam()
     ps_gen = pspec.PowerSpectraCart(eor, ps_conf, pb)
-    print(ps_gen.config.umax)
-    print(ps_gen.config.umin)
+
+    k_par_centre = K_PAR
+    dk_par_centre = np.diff(k_par_centre)
+    k_par_bins = np.concatenate([[k_par_centre[0]-dk_par_centre[0]/2],k_par_centre[:-1]+dk_par_centre/2,[k_par_centre[-1]+dk_par_centre[-1]/2]])
     
     ps2d = ps_gen.get_ps2d(data_cube)
     k_par = ps2d.k_par
     k_per = ps2d.k_per
     ps_data = ps2d.data
     ps_err = ps2d.err
+    binned_data = np.zeros((len(k_par_centre),len(k_per)))
+    binned_error = np.zeros((len(k_par_centre),len(k_per)))
     
-    ix, iy = np.where(~np.isnan(ps_data))
-    iy = np.unique(iy)
-    ps_data = ps_data[:,iy]
-    ps_err = ps_err[:,iy]
-    k_per = k_per[iy]
-    # print(ps_data.max())
-    # print(ps_data.min())
+    for i in range(len(k_per)):
+        binned_data[:,i] = stats.binned_statistic(k_par,ps_data[:,i],bins=k_par_bins).statistic
+        binned_error[:,i] = stats.binned_statistic(k_par,ps_err[:,i],bins=k_par_bins,statistic=error_prop).statistic
+    assert binned_data.shape == binned_error.shape
+    print(binned_data.shape)
     
-    freq_inteval = str(freq_lower)+'_'+str(freq_upper)+'MHz'
-    
-    ps2d.save_to_txt(outpath+'ps2d_'+freq_inteval+'.txt')
+    if MASK:
+        k_per,k_par_centre,binned_data,binned_error
+        for icol in range(binned_data.shape[1]):
+            col = binned_data[:,icol]
+            col_ratios = ratio(col)
+            i_mask = np.where(col_ratios > THRESHOLD)[0][-1]
+            if (icol < 5) & (i_mask > 2):
+                i_mask = 2
+            binned_data[:,icol][:i_mask+1] = np.nan
+            binned_error[:,icol][:i_mask+1] = np.nan
+
     plt.figure()
-    plt.pcolormesh(k_per,k_par,ps_data,norm='log',cmap='jet')
+    plt.pcolormesh(k_per,k_par_centre,binned_data,norm='log',cmap='jet')
     plt.xlim(k_per.min(),k_per.max())
-    plt.ylim(k_par.min(),k_par.max())
-    plt.xlabel('$k_\perp\ [h\ \mathrm{cMpc^{-1}}$]')
-    plt.ylabel('$k_\parallel\ [h\ \mathrm{cMpc^{-1}}$]')
+    plt.ylim(k_par_centre.min(),k_par_centre.max())
     plt.xscale('log')
     plt.yscale('log')
-    plt.title('$P_\mathrm{k}$')
-    plt.colorbar(label='$P_\mathrm{k}\ [\mathrm{K^2}\ h^{-1}\ \mathrm{cMpc}^3$]')
-    plt.savefig(outpath+'ps2d_{}_'.format(kind)+freq_inteval+'.pdf',bbox_inches='tight')
+    plt.colorbar()
+    plt.savefig(outpath+'ps2d_res_{}_{}MHz'.format(freq_lower,freq_upper)+str(weighting)+'.png',bbox_inches='tight')
     
     plt.figure()
-    plt.pcolormesh(k_per,k_par,ps_err,norm='log',cmap='jet')
+    plt.pcolormesh(k_per,k_par_centre,binned_error,norm='log',cmap='jet')
     plt.xlim(k_per.min(),k_per.max())
-    plt.ylim(k_par.min(),k_par.max())
-    plt.xlabel('$k_\perp\ [h\ \mathrm{cMpc^{-1}}$]')
-    plt.ylabel('$k_\parallel\ [h\ \mathrm{cMpc^{-1}}$]')
+    plt.ylim(k_par_centre.min(),k_par_centre.max())
     plt.xscale('log')
     plt.yscale('log')
-    plt.title('$\Delta P_\mathrm{k}$')
-    plt.colorbar(label='$\Delta P_\mathrm{k}\ [\mathrm{K^2}\ h^{-1}\ \mathrm{cMpc}^3$]')
-    plt.savefig(outpath+'ps2d_err_{}_'.format(kind)+freq_inteval+'.pdf',bbox_inches='tight')
+    plt.colorbar()
+    plt.savefig(outpath+'ps2d_err_res_{}_{}MHz'.format(freq_lower,freq_upper)+str(weighting)+'.png',bbox_inches='tight')
     
-    # kbins = np.logspace(np.log10(ps_gen.kmin), np.log10(ps_gen.all_k.max()), 50)
-    # ps1d = ps_gen.get_ps3d(kbins, data_cube)
-    # ps1d.plot(title='Spherically averaged power spectra',kerr_as_kbins=ERROR_P1D)
-    # plt.savefig(outpath+'ps1d_'+'.pdf')
-    # ps1d.save_to_txt(outpath+'ps1d_'+'.txt')
+    ### Submission
+    np.savetxt(outpath+'Cantabrigians_{:.1f}MHz-{:.1f}MHz.data'.format(freq_lower,freq_upper),binned_data)
+    np.savetxt(outpath+'Cantabrigians_{:.1f}MHz-{:.1f}MHz_errors.data'.format(freq_lower,freq_upper),binned_error)
     
 processes = []
 for i in res_cubes:
